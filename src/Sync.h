867d743abc2ec70baf6391ad70673dd25cd64a5a
Matt Daley <m6daley@gmail.com>
2020-08-21 12:23:55 +0100

    Missing things put in.  Still doesn't work though.

--- src/Sync.h
+++ src/Sync.h
@@ -1,9 +1,16 @@
 #ifndef TIMER_SYNC_H
 #define TIMER_SYNC_H
 
+#include <mutex>
 
 class Sync {
-
+public:
+    Sync(std::mutex* mutex_, std::condition_variable* condVar_, unsigned int interval_) :
+            mutex{mutex_}, condVar{condVar_}, state{false}, interval{interval_} {};
+    std::mutex* mutex;
+    std::condition_variable* condVar;
+    bool state;
+    unsigned int interval;
 };
 
 
--- test/RootTimerTests.cpp
+++ test/RootTimerTests.cpp
@@ -1,6 +1,42 @@
+#include <RootTimer.h>
+#include <map>
+#include <Sync.h>
 #include "catch.hpp"
 
-TEST_CASE("Testing catch") {
-    CHECK(true);
-}
+TEST_CASE("Add children syncs works, children syncs can be retrieved") {
+    RootTimer rt(1000);
 
+    std::mutex mu1;
+    std::mutex mu2;
+    std::condition_variable cv1;
+    std::condition_variable cv2;
+    Sync sync1(&mu1, &cv1, 1);
+    Sync sync2(&mu2, &cv2, 1);
+
+    /*sync1.interval = std::make_shared<unsigned int>(7);
+
+    std::map<int, Sync> m;
+
+    m.emplace(1, sync1);
+
+    Sync s = m.at(1);
+    std::cout << *(s.interval.get()) << std::endl;
+
+    *(s.interval.get()) = 2;
+
+    std::cout << *(sync1.interval.get()) << std::endl;
+
+    */
+    std::map<int, std::shared_ptr<Sync>> m;
+
+    std::mutex mu;
+    m.emplace(1, std::make_shared<Sync>(sync1));
+    m.emplace(2, std::make_shared<Sync>(sync1));
+
+    std::cout << sync1.interval << std::endl;
+
+    std::shared_ptr<Sync> z = m.at(1);
+    z.get()->interval = 7;
+
+    std::cout << sync1.interval << std::endl;
+}
